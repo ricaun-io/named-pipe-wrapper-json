3c7a3ca0ba7ddab6b2d84c10ad2ace4d8f20ee14
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2023-01-24 10:02:51 -0300

    Update Nuke

--- Build/Build.cs
+++ Build/Build.cs
@@ -1,10 +1,35 @@
 using Nuke.Common;
 using Nuke.Common.Execution;
+using Nuke.Common.Tools.GitHub;
 using ricaun.Nuke;
 using ricaun.Nuke.Components;
 
-internal class Build : NukeBuild, IPublishPack, ITest
+internal class Build : NukeBuild, IPublishPack, ITest, IShowGitVersion
 {
     string ITest.TestProjectName => "UnitTests";
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
+}
+
+public interface IShowGitVersion : IHazGitVersion, IHazChangelog, IHazGitRepository, IClean
+{
+    Target ShowGitVersion => _ => _
+        .TriggeredBy(Clean)
+        .Requires(() => GitRepository)
+        .Executes(() =>
+        {
+            // GitVersion.BranchName
+            Serilog.Log.Information(GitVersion.BranchName);
+
+            // RootDirectory
+            Serilog.Log.Information(RootDirectory);
+
+            var gitHubName = GitRepository.GetGitHubName();
+            var gitHubOwner = GitRepository.GetGitHubOwner();
+
+            Serilog.Log.Information(gitHubName);
+            Serilog.Log.Information(gitHubOwner);
+
+            // GetReleaseNotes
+            Serilog.Log.Information(GetReleaseNotes());
+        });
 }
